0110a49434e23894bec8eecf38079ddc2e713eed
Peckoij <jonpekkonen@gmail.com>
2021-10-30 10:35:28 +0300

    React frontin build lisätty, ja urlit korjattu jotta sovelluksen pitäisi toimia herokun kautta

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 // Morgan token to be used in POST logging
 morgan.token('typePost', function (req, res) { return JSON.stringify(req.body) })
